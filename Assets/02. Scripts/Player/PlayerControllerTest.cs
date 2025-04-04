9d8e4625c6b905143e5e24ee324b39863932e687
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-04-05 05:41:03 +0900

    [Feat] PlayerControllerTest.cs추가
    해당 스크립트에서는 플레이어의 기본적인 이동과 점프만 구현된 스크립트입니다.
    임시 스크립트이기에 플레이어의 상태머신이 구현된다면 삭제될 예정입니다.

---
+++ Assets/02. Scripts/Player/PlayerControllerTest.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerTest : MonoBehaviour
+{
+    //TODO: 테스트 스크립트, 추후 유한상태머신 구현 후 삭제 예정
+    private Player player;
+
+    private void Awake()
+    {
+        player = GetComponent<Player>();
+    }
+
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+
+    private Vector2 ComputeMove()
+    {
+        Vector2 moveVector;
+        if(player.input.IsJump && player.playerCheckGround.isGround)
+        {
+            player.playerRigidbody.AddForce(player.playerData.jumpForce * Vector2.up, ForceMode2D.Impulse);
+        }
+
+        moveVector = new Vector2(player.input.MoveDir.x * player.playerData.speed, player.playerRigidbody.velocity.y);
+        return moveVector;
+    }
+
+
+    private void Move()
+    {
+        player.playerRigidbody.velocity = ComputeMove();
+    }
+}
