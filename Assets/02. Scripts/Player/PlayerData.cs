b8b6feb1a63dde0e5bfe1c71aa81b6d5cb036561
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-04-05 05:41:56 +0900

    [Feat] PlayerData.cs
    해당 스크립트는 플레이어의 다양한 정보를 담고 있는 스크립트입니다.
    
    현재는 간단하게 플레이어의 이동속도와 점프의 힘의 정보만 가지며 직렬화를 통해 인스펙터 창에서 조절 가능하게 만들었습니다.

---
+++ Assets/02. Scripts/Player/PlayerData.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class PlayerData
+{
+    public float speed = 50f;
+    public float jumpForce = 5f;
+}
