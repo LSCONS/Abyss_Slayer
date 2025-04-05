930001d8b8ca1b34a430f1d8bb596aa9510bad77
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-04-06 01:24:04 +0900

    [Feat] PlayerIdleState.cs 수정
    WalkState, JumpState, FallState로 진입할 수 있도록 기반 추가

--- Assets/02. Scripts/Player/StateMachine/PlayerIdleState.cs
+++ Assets/02. Scripts/Player/StateMachine/PlayerIdleState.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,5 +7,43 @@ public class PlayerIdleState : PlayerGroundState
 {
     public PlayerIdleState(PlayerStateMachine playerStateMachine) : base(playerStateMachine)
     {
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+        playerStateMachine.MovementSpeed = 0f;
+        Debug.Log("IdleState 진입");
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+        playerStateMachine.MovementSpeed = playerStateMachine.Player.playerData.PlayerGroundData.BaseSpeed;
+        Debug.Log("IdleState 해제");
+    }
+
+    public override void Update()
+    {
+        base.Update();
+        if(playerStateMachine.Player.input.MoveDir.x != 0f)
+        {
+            playerStateMachine.ChangeState(playerStateMachine.WalkState);
+            return;
+        }
+
+        if (playerStateMachine.Player.input.IsJump && 
+            playerStateMachine.Player.playerCheckGround.CanJump &&
+            Mathf.Approximately(playerStateMachine.Player.playerRigidbody.velocity.y, 0))
+        {
+            playerStateMachine.ChangeState(playerStateMachine.JumpState);
+            return;
+        }
+
+        if (!(playerStateMachine.Player.playerCheckGround.CanJump))
+        {
+            playerStateMachine.ChangeState(playerStateMachine.FallState);
+            return;
+        }
     }
 }
