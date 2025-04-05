ff8fddde6d3823ec5b8222c4253bbdf820eb8939
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-04-05 17:01:12 +0900

    [Docs] PlayerCheckGround.cs 주석 추가

--- Assets/02. Scripts/Player/PlayerCheckGround.cs
+++ Assets/02. Scripts/Player/PlayerCheckGround.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class PlayerCheckGround : MonoBehaviour
 {
-    public bool isGround = false;
+    public bool isGround = false;   //플레이어가 땅을 확인하는 Collider로 땅에 닿았는지 체크해주는 변수
+
+    //Collider가 땅에 닿을 경우 isGround true반환.
     private void OnTriggerEnter2D(Collider2D collision)
     {
-
         if(collision.gameObject.layer == LayerData.GroundLayerIndex)
         {
             isGround = true;
         }
     }
 
+    //Collider가 땅에서 빠져나갈 경우 isGround false반환.
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.layer == LayerData.GroundLayerIndex)
