2fcc0770a93371331ccd0bf31e388da6d3bed19c
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-04-05 16:37:17 +0900

    [Feat] PlayerAirState.cs 추가
    나중에 점프상태나 떨어지는 상태에 돌입할 때 부모 클래스로 관리할 수 있는 PlayerAirState를 추가했습니다.
    
    아직 아무런 기능이 없습니다.

---
+++ Assets/02. Scripts/Player/StateMachine/PlayerAirState.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAirState : PlayerBaseState
+{
+    public PlayerAirState(PlayerStateMachine playerStateMachine) : base(playerStateMachine)
+    {
+    }
+}
