86e5cc5c4fd63d0d23ebc5b65fc92d857a7800b5
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-04-06 01:17:19 +0900

    [Feat] PlayerAirState.cs 수정
    진입 여부를 확인할 수 있게 기반을 제작.
    
    Debug.Log도 추가

--- Assets/02. Scripts/Player/StateMachine/PlayerAirState.cs
+++ Assets/02. Scripts/Player/StateMachine/PlayerAirState.cs
@@ -7,4 +7,18 @@ public class PlayerAirState : PlayerBaseState
     public PlayerAirState(PlayerStateMachine playerStateMachine) : base(playerStateMachine)
     {
     }
+
+    public override void Enter()
+    {
+        base.Enter();
+        //TODO: Air파라미터 활성화 필요
+        Debug.Log("AirState 진입");
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+        //TODO: Air파라미터 비활성화 필요
+        Debug.Log("AirState 해제");
+    }
 }
