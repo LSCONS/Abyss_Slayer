86bf5c17abfbcf659383a04f686700efadd295e8
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-04-06 01:22:50 +0900

    [Test] PlayerAirState.cs 수정
    Debug.Log가 너무 많아 방해되어 삭제

--- Assets/02. Scripts/Player/StateMachine/PlayerAirState.cs
+++ Assets/02. Scripts/Player/StateMachine/PlayerAirState.cs
@@ -12,13 +12,11 @@ public class PlayerAirState : PlayerBaseState
     {
         base.Enter();
         //TODO: Air파라미터 활성화 필요
-        Debug.Log("AirState 진입");
     }
 
     public override void Exit()
     {
         base.Exit();
         //TODO: Air파라미터 비활성화 필요
-        Debug.Log("AirState 해제");
     }
 }
