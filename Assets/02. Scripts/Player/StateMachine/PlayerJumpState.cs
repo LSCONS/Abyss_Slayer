8ad3d5cdca8d671bbbf846e053bdc72f445aa106
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-04-06 01:25:09 +0900

    [Feat] PlayerJumpState.cs 수정
    Enter로 접속 시 점프하는 명렁어 추가.
    
    Rigidbody.velocity.y의 값이 0보다 같거나 적으면 FallState로 진입할 수 있도록 추가.

--- Assets/02. Scripts/Player/StateMachine/PlayerJumpState.cs
+++ Assets/02. Scripts/Player/StateMachine/PlayerJumpState.cs
@@ -7,4 +7,28 @@ public class PlayerJumpState : PlayerAirState
     public PlayerJumpState(PlayerStateMachine playerStateMachine) : base(playerStateMachine)
     {
     }
+
+    public override void Enter()
+    {
+        base.Enter();
+        //TODO: 점프 애니메이션 실행
+        Jump();
+        Debug.Log("JumpState 진입");
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+        //TODO: 점프 애니메이션 종료
+        Debug.Log("JumpState 해제");
+    }
+
+    public override void Update()
+    {
+        base.Update();
+        if(playerStateMachine.Player.playerRigidbody.velocity.y <= 0)
+        {
+            playerStateMachine.ChangeState(playerStateMachine.FallState);
+        }
+    }
 }
