139ae74cd8b87049ea784c1ed84da455ce92ac88
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-04-06 01:21:44 +0900

    [Feat] PlayerGroundState.cs 수정
    플레이어가 땅에 닿고 있을 때 실행될 GroundState의 기반을 완성.

--- Assets/02. Scripts/Player/StateMachine/PlayerGroundState.cs
+++ Assets/02. Scripts/Player/StateMachine/PlayerGroundState.cs
@@ -11,6 +11,14 @@ public class PlayerGroundState : PlayerBaseState
     public override void Enter()
     {
         base.Enter();
-        
+        //TODO: 애니메이션 파라미터 @Ground True 변경
+        Debug.Log("GroundState 진입");
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+        //TODO: 애니매이션 파라미터 @Ground True 변경
+        Debug.Log("GroundState 해제");
     }
 }
