5a3c39f0bd9cf9ccdc02dc386205197f1844235a
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-04-05 16:39:03 +0900

    [Feat] PlayerGroundState.cs 추가
    플레이어가 땅에 있는 상태에서 다양한 작동을 할 때 부모 클래스에서 이들을 관리해줄 State입니다.
    
    아직 아무런 기능도 추가하지 않았습니다.

---
+++ Assets/02. Scripts/Player/StateMachine/PlayerGroundState.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGroundState : PlayerBaseState
+{
+    public PlayerGroundState(PlayerStateMachine playerStateMachine) : base(playerStateMachine)
+    {
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+        
+    }
+}
