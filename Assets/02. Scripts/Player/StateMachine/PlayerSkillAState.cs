5d65ea3e10c355882a09ced2cd127854d951e14e
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-04-07 18:59:13 +0900

    [Feat] PlayerSkillAState.cs 추가
    A스킬을 사용할 State추가

---
+++ Assets/02. Scripts/Player/StateMachine/PlayerSkillAState.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSkillAState : PlayerAttackState
+{
+    public PlayerSkillAState(PlayerStateMachine playerStateMachine) : base(playerStateMachine)
+    {
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+}
