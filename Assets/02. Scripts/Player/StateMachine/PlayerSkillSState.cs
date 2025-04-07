507fdd362763a9e9bb8ece406ac6bc343567b8f7
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-04-07 19:00:17 +0900

    [Feat] PlayerSkillSState.cs 추가
    S스킬에 진입할 수 있는 State 추가

---
+++ Assets/02. Scripts/Player/StateMachine/PlayerSkillSState.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSkillSState : PlayerAttackState
+{
+    public PlayerSkillSState(PlayerStateMachine playerStateMachine) : base(playerStateMachine)
+    {
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+}
