d05023f7252771cbabad04444867e9974b28b14a
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-04-07 20:58:04 +0900

    [Feat] PlayerData.cs 추가
    플레이어 데이터에 대시 힘 추가

--- Assets/02. Scripts/Player/PlayerData.cs
+++ Assets/02. Scripts/Player/PlayerData.cs
@@ -17,6 +17,9 @@ public class PlayerAirData
     [field: Header("JumpData")]
     [field: SerializeField] public float WalkSpeedModifier { get; private set; } = 0.225f;
     [field: SerializeField] public float JumpForce { get; private set; } = 10f;
+
+    [field: Header("DashData")]
+    [field: SerializeField] public float DashForce { get; private set; } = 5f;
 }
 
 
@@ -26,8 +29,17 @@ public class PlayerGroundData
     [field: Header("IdleData")]
     [field: SerializeField] public float BaseSpeed { get; private set; } = 10f;
     [field: SerializeField] public float BaseRotationDamping { get; private set; } = 1f;
+
     [field: Header("WalkData")]
     [field: SerializeField] public float WalkSpeedModifier { get; private set; } = 0.225f;
+
     [field: Header("RunData")]
     [field: SerializeField] public float RunSpeedModifier { get; private set; } = 1f;
+}
+
+
+[Serializable]
+public class PlayerAbnormal
+{
+
 }
