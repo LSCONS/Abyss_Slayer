428e423a335c6b063aabb37c3de85822349e4d21
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-04-07 21:02:17 +0900

    [Feat] PlayerCommonAttackState.cs 추가
    스크립트 주석 추가 및 기반 작성 중

--- Assets/02. Scripts/Player/StateMachine/PlayerCommonAttackState.cs
+++ Assets/02. Scripts/Player/StateMachine/PlayerCommonAttackState.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerCommonAttackState : PlayerAttackState
 {
+    //TODO: PlayerX스킬 데이터 가져와야함.
     public PlayerCommonAttackState(PlayerStateMachine playerStateMachine) : base(playerStateMachine)
     {
     }
@@ -11,10 +12,19 @@ public class PlayerCommonAttackState : PlayerAttackState
     public override void Enter()
     {
         base.Enter();
+        //TODO: 스킬 사용 중 움직일 수 있는지 확인하고 실행
+        //TODO: 스킬 X 애니메이터 활성화
     }
 
     public override void Exit()
     {
         base.Exit();
+        //TODO: 스킬 X 애니메이터 비활성화
+        playerStateMachine.MovementSpeed = playerStateMachine.Player.playerData.PlayerGroundData.BaseSpeed;
+    }
+
+    public override void Update()
+    {
+        base.Update();
     }
 }
