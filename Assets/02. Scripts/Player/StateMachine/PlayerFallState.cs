b0061d78b2a594b96912c25de47bf02abcb57d34
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-04-07 18:57:36 +0900

    [Feat] PlayerFallState.cs 추가
    AttackState로 넘어갈 수 있는 AttackAction추가

--- Assets/02. Scripts/Player/StateMachine/PlayerFallState.cs
+++ Assets/02. Scripts/Player/StateMachine/PlayerFallState.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerFallState : PlayerAirState
 {
+    public Action AttackAction;
     public PlayerFallState(PlayerStateMachine playerStateMachine) : base(playerStateMachine)
     {
     }
@@ -30,5 +32,7 @@ public class PlayerFallState : PlayerAirState
         {
             playerStateMachine.ChangeState(playerStateMachine.IdleState);
         }
+
+        AttackAction.Invoke();
     }
 }
