33bc81e95bf700260c90263b1a70aeee6bd36dcf
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-04-07 21:04:32 +0900

    [Feat] PlayerSkillSState.cs 추가
    플레이어 S스킬 State 기반 작성 중

--- Assets/02. Scripts/Player/StateMachine/PlayerSkillSState.cs
+++ Assets/02. Scripts/Player/StateMachine/PlayerSkillSState.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerSkillSState : PlayerAttackState
 {
+    //TODO: PlayerS스킬 데이터 가져와야함.
     public PlayerSkillSState(PlayerStateMachine playerStateMachine) : base(playerStateMachine)
     {
     }
@@ -11,10 +12,14 @@ public class PlayerSkillSState : PlayerAttackState
     public override void Enter()
     {
         base.Enter();
+        //TODO: 스킬 사용 중 움직일 수 있는지 확인하고 실행
+        //TODO: 스킬 S 애니메이터 활성화
     }
 
     public override void Exit()
     {
         base.Exit();
+        //TODO: 스킬 S 애니메이터 비활성화
+        playerStateMachine.MovementSpeed = playerStateMachine.Player.playerData.PlayerGroundData.BaseSpeed;
     }
 }
