261534dec8dfaba041c4b0a44b70c29f1f7bba8e
Jung Ha Na <73771420+onehana1@users.noreply.github.com>
2025-04-09 20:54:22 +0900

    [Feat] UIHealthBar 스크립트 수정
    # UIHealthBar.cs
    - SetHp fillAmount 설정해주는 메서드 추가
    - SetPresenter로 presenter 연결하는 메서드 추가
    - OnDestroy시 presenter의 Dispose 실행

--- Assets/02. Scripts/UI/UIHealthBar.cs
+++ Assets/02. Scripts/UI/UIHealthBar.cs
@@ -2,47 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UniRx;
 
-public class UIHealthBar : UIBase
+public class UIHealthBar : UIBase, IView
 {
     [SerializeField] private Image hpBar;
-    [SerializeField] private string targetName;
-    [SerializeField] private Transform target;
+    [SerializeField] private IPresenter presenter;
 
-    private IHasHealth healthTarget;
-    private void OnValidate()
-    {
+
+    private void OnValidate() {
         hpBar = transform.GetGameObjectSameNameDFS("HpBar").GetComponent<Image>();
-        // 전체에서 오브젝트 찾기
-        target = GameObject.Find(targetName).transform;  // 타겟 이름 받아서 OnValidate 에서도 가능하게
     }
+
     public override void Init()
     {
-        this.gameObject.SetActive(true);
         hpBar.fillAmount = 1;
-        if (target == null)
-        {
-            Debug.LogError($"UIHealthBar {targetName} 찾을 수 없음");
-        }
-        if (!target.TryGetComponent<IHasHealth>(out healthTarget))
-        {
-            Debug.LogError($"[UIHealthBar] {targetName} 찾을 수 없음");
-            return;
-        }
+    }
 
-        healthTarget.OnHpChanged += SetHp;
+    public void SetHp(float value)
+    {
+        hpBar.fillAmount = value;
+    }
+
+    public void SetPresenter(IPresenter presenter)
+    {
+        this.presenter = presenter;
     }
 
     private void OnDestroy()
     {
-        if (healthTarget != null)
-        {
-            healthTarget.OnHpChanged -= SetHp;
-        }
+        presenter?.Dispose();
     }
-    public void SetHp(int hp, int maxHp)
-    {
-        hpBar.fillAmount = Mathf.Clamp01((float)hp / maxHp);
-    }
+
 
 }
