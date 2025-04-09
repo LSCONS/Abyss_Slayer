50a5a26b5dea6139e89b92d885692cd6b329cbc7
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-04-09 19:07:55 +0900

    [Refactor] PlayerWalkState.cs 수정
    MoveAction에 엮어서 코드 복잡도 감소

--- Assets/02. Scripts/Player/StateMachine/PlayerWalkState.cs
+++ Assets/02. Scripts/Player/StateMachine/PlayerWalkState.cs
@@ -5,9 +5,18 @@ using UnityEngine;
 
 public class PlayerWalkState : PlayerGroundState
 {
+    public StoppableAction MoveAction = new();
     public StoppableAction AttackAction = new();
     public PlayerWalkState(PlayerStateMachine playerStateMachine) : base(playerStateMachine)
     {
+        //Idle 스테이트 진입 가능 여부 확인
+        MoveAction.AddListener(playerStateMachine.ConnectIdleState);
+        //Jump 스테이트 진입 가능 여부 확인
+        MoveAction.AddListener(playerStateMachine.ConnectJumpState);
+        //Fall 스테이트 진입 가능 여부 확인
+        MoveAction.AddListener(playerStateMachine.ConnectFallState);
+        //Dash 스테이트 진입 가능 여부 확인
+        MoveAction.AddListener(playerStateMachine.ConnectDashState);
     }
 
     public override void Enter()
@@ -32,53 +41,7 @@ public class PlayerWalkState : PlayerGroundState
     public override void Update()
     {
         base.Update();
-
-        //Idle 스테이트 진입 가능 여부 확인
-        if (playerStateMachine.Player.input.MoveDir == Vector2.zero)
-        {
-            playerStateMachine.ChangeState(playerStateMachine.IdleState);
-            return;
-        }
-
-        //Jump 스테이트 진입 가능 여부 확인
-        if (playerStateMachine.Player.input.IsJump &&
-            playerStateMachine.Player.playerCheckGround.CanJump &&
-            Mathf.Approximately(playerStateMachine.Player.playerRigidbody.velocity.y, 0))
-        {
-            if (playerStateMachine.Player.input.MoveDir.y < 0 &&
-                playerStateMachine.Player.playerCheckGround.GroundPlaneCount == 0)
-            {
-                //TODO: 다운점프 로직 시작
-                playerStateMachine.Player.playerGroundCollider.isTrigger = true;
-                playerStateMachine.ChangeState(playerStateMachine.FallState);
-                return;
-            }
-            else if (playerStateMachine.Player.input.MoveDir.y >= 0)
-            {
-                playerStateMachine.ChangeState(playerStateMachine.JumpState);
-                return;
-            }
-        }
-
-        //Fall 스테이트 진입 가능 여부 확인
-        if (!(playerStateMachine.Player.playerCheckGround.CanJump))
-        {
-            playerStateMachine.ChangeState(playerStateMachine.FallState);
-            return;
-        }
-
-        //Dash 스테이트 진입 가능 여부 확인
-        if (playerStateMachine.Player.playerData.PlayerAirData.CanDash &&
-            playerStateMachine.Player.input.IsDash &&
-            (playerStateMachine.Player.input.MoveDir.x != 0 ||
-            playerStateMachine.Player.input.MoveDir.y > 0) &&
-            playerStateMachine.Player.playerData.PlayerAirData.CurDashCount > 0 &&
-            playerStateMachine.Player.input.MoveDir != Vector2.zero)
-        {
-            playerStateMachine.ChangeState(playerStateMachine.DashState);
-            return;
-        }
-
+        MoveAction?.Invoke();
         AttackAction?.Invoke();
     }
 }
