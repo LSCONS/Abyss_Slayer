4c5a74ae1aa2c500c0e1d9efea4bf31af2b28e45
Jung Ha Na <73771420+onehana1@users.noreply.github.com>
2025-04-09 20:55:20 +0900

    [Feat] HPTest에 hp를 ReactiveProperty로 수정
    - MVRP 패턴을 위해 ReactiveProperty로 수정

--- Assets/WorkSpace/JHN/Scripts/HPTest.cs
+++ Assets/WorkSpace/JHN/Scripts/HPTest.cs
@@ -2,27 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
-
+using UniRx;
 
 
 public class HPTest : MonoBehaviour, IHasHealth
 {   
-    public int hp = 100;
-    public int maxHp = 100;
-    public event Action<int, int> OnHpChanged;
+    public ReactiveProperty<int> Hp { get; private set; } = new ReactiveProperty<int>(100);
+    public ReactiveProperty<int> MaxHp { get; private set; } = new ReactiveProperty<int>(100);
 
     private void Update() {
         if (Input.GetKeyDown(KeyCode.H))
         {
-            hp = Mathf.Max(hp - 5, 0);
-            SetHp(hp, maxHp);
+            Hp.Value = Mathf.Max(Hp.Value - 5, 0);
         }
 
-    }
-    public void SetHp(int hp, int maxHp)
-    {
-        this.hp = hp;
-        this.maxHp = maxHp;
-        OnHpChanged?.Invoke(hp, maxHp);
     }
 }
