8de528bc3f8319ecc58a14c8fac8f3c68aba6039
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-04-10 19:59:15 +0900

    [Feat] PlayerAnimationData.cs 추가
    AnimationHash값도 추가

--- Assets/02. Scripts/Player/PlayerAnimationData.cs
+++ Assets/02. Scripts/Player/PlayerAnimationData.cs
@@ -6,34 +6,47 @@ using UnityEngine;
 [Serializable]
 public class PlayerAnimationData
 {
-    [SerializeField] private string groundParameterName         = "@Ground";
-    [SerializeField] private string idleParameterName           = "Idle";
-    [SerializeField] private string walkParameterName           = "Walk";
+    [SerializeField] private string groundParameterName     = "@Ground";
+    [SerializeField] private string idleParameterName       = "Idle";
+    [SerializeField] private string walkParameterName       = "Walk";
 
-    [SerializeField] private string airParematerName            = "@Air";
-    [SerializeField] private string jumpParameterName           = "Jump";
-    [SerializeField] private string fallParameterName           = "Fall";
+    [SerializeField] private string airParematerName        = "@Air";
+    [SerializeField] private string jumpParameterName       = "Jump";
+    [SerializeField] private string fallParameterName       = "Fall";
 
-    [SerializeField] private string attackParameterName         = "@Skill";
-    [SerializeField] private string commonAttackParameterName   = "CommonAttack";
-    [SerializeField] private string Z_SkillParameterName        = "Z_Skill";
-    [SerializeField] private string A_SkillParameterName        = "A_Skill";
-    [SerializeField] private string S_SkillParameterName        = "S_Skill";
-    [SerializeField] private string D_SkillParameterName        = "D_Skill";
+    [SerializeField] private string skillParameterName      = "@Skill";
+    [SerializeField] private string X_SkillParameterName    = "X_Skill";
+    [SerializeField] private string Z_SkillParameterName    = "Z_Skill";
+    [SerializeField] private string A_SkillParameterName    = "A_Skill";
+    [SerializeField] private string S_SkillParameterName    = "S_Skill";
+    [SerializeField] private string D_SkillParameterName    = "D_Skill";
 
-    public int groundParameterHash          { get; private set; }
-    public int idleParameterHash            { get; private set; }
-    public int walkParameterHash            { get; private set; }
-    public int airParematerHash             { get; private set; }
-    public int jumpParameterHash            { get; private set; }
-    public int fallParameterHash            { get; private set; }
-    public int attackParameterHash          { get; private set; }
-    public int commonAttackParameterHash    { get; private set; }
-    public int Z_SkillParameterHash         { get; private set; }
-    public int A_SkillParameterHash         { get; private set; }
-    public int S_SkillParameterHash         { get; private set; }
-    public int D_SkillParameterHash         { get; private set; }
+    [SerializeField] private string Z_SkillAnimationName    = "Player_SkillA";
+    [SerializeField] private string X_SkillAnimationName    = "Player_SkillX";
+    [SerializeField] private string A_SkillAnimationName    = "Player_SkillA";
+    [SerializeField] private string S_SkillAnimationName    = "Player_SkillS";
+    [SerializeField] private string D_SkillAnimationName    = "Player_SkillD";
 
+    public int groundParameterHash      { get; private set; }
+    public int idleParameterHash        { get; private set; }
+    public int walkParameterHash        { get; private set; }
+
+    public int airParematerHash         { get; private set; }
+    public int jumpParameterHash        { get; private set; }
+    public int fallParameterHash        { get; private set; }
+
+    public int skillParameterHash       { get; private set; }
+    public int X_SkillParameterHash     { get; private set; }
+    public int Z_SkillParameterHash     { get; private set; }
+    public int A_SkillParameterHash     { get; private set; }
+    public int S_SkillParameterHash     { get; private set; }
+    public int D_SkillParameterHash     { get; private set; }
+
+    public int Z_SkillAnimationHash     { get; private set; }
+    public int X_SkillAnimationHash     { get; private set; }
+    public int A_SkillAnimationHash     { get; private set; }
+    public int S_SkillAnimationHash     { get; private set; }
+    public int D_SkillAnimationHash     { get; private set; }
 
     public PlayerAnimationData()
     {
@@ -42,17 +55,25 @@ public class PlayerAnimationData
 
     private void Init()
     {
-        groundParameterHash =Animator.StringToHash( groundParameterName);
-        idleParameterHash = Animator.StringToHash(idleParameterName);        
-        walkParameterHash = Animator.StringToHash(walkParameterName);
-        airParematerHash = Animator.StringToHash(airParematerName);
-        jumpParameterHash = Animator.StringToHash(jumpParameterName);       
-        fallParameterHash = Animator.StringToHash(fallParameterName);
-        attackParameterHash = Animator.StringToHash(attackParameterName);
-        commonAttackParameterHash = Animator.StringToHash(commonAttackParameterName);
-        Z_SkillParameterHash = Animator.StringToHash(Z_SkillParameterName);     
-        A_SkillParameterHash = Animator.StringToHash(A_SkillParameterName);     
-        S_SkillParameterHash = Animator.StringToHash(S_SkillParameterName);     
-        D_SkillParameterHash = Animator.StringToHash(D_SkillParameterName);     
+        groundParameterHash     = Animator.StringToHash(groundParameterName);
+        idleParameterHash       = Animator.StringToHash(idleParameterName);        
+        walkParameterHash       = Animator.StringToHash(walkParameterName);
+
+        airParematerHash        = Animator.StringToHash(airParematerName);
+        jumpParameterHash       = Animator.StringToHash(jumpParameterName);       
+        fallParameterHash       = Animator.StringToHash(fallParameterName);
+
+        skillParameterHash      = Animator.StringToHash(skillParameterName);
+        X_SkillParameterHash    = Animator.StringToHash(X_SkillParameterName);
+        Z_SkillParameterHash    = Animator.StringToHash(Z_SkillParameterName);     
+        A_SkillParameterHash    = Animator.StringToHash(A_SkillParameterName);     
+        S_SkillParameterHash    = Animator.StringToHash(S_SkillParameterName);     
+        D_SkillParameterHash    = Animator.StringToHash(D_SkillParameterName);
+
+        Z_SkillAnimationHash    = Animator.StringToHash(Z_SkillAnimationName);
+        X_SkillAnimationHash    = Animator.StringToHash(X_SkillAnimationName);
+        A_SkillAnimationHash    = Animator.StringToHash(A_SkillAnimationName);
+        S_SkillAnimationHash    = Animator.StringToHash(S_SkillAnimationName);
+        D_SkillAnimationHash    = Animator.StringToHash(D_SkillAnimationName);
     }                                               
 }                                                   
