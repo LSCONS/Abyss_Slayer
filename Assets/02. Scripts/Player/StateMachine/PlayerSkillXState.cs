5813c53e5e3e4996ffa36b9b1198c9048705e37f
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-04-10 19:58:32 +0900

    [Rename] PlayerCommonAttackState.cs 이름 변경
    PlayerSkillXState.cs로 변경

---
+++ Assets/02. Scripts/Player/StateMachine/PlayerSkillXState.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSkillXState : PlayerSkillState
+{
+    private SkillData SkillData { get; set; }
+    private SkillSlotKey slotkey { get; set; } = SkillSlotKey.X;
+    public PlayerSkillXState(PlayerStateMachine playerStateMachine) : base(playerStateMachine)
+    {
+        SkillData = playerStateMachine.Player.equippedSkills[slotkey];
+        playerStateMachine.ConnectSkillState(this, SkillData,() => playerStateMachine.Player.input.IsAttack);
+    }
+    public override void Enter()
+    {
+        base.Enter();
+        StartAnimation(playerStateMachine.Player.playerAnimationData.X_SkillParameterHash);
+        if (!(SkillData.canMove)) playerStateMachine.MovementSpeed = 0f;
+        playerStateMachine.IsCompareState = false;
+        playerStateMachine.Player.SkillCoolTimeUpdate(slotkey);
+
+#if StateMachineDebug
+        Debug.Log("SkillXState 진입");
+#endif
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+        StopAnimation(playerStateMachine.Player.playerAnimationData.X_SkillParameterHash);
+        playerStateMachine.MovementSpeed = playerStateMachine.Player.playerData.PlayerGroundData.BaseSpeed;
+
+#if StateMachineDebug
+        Debug.Log("SkillXState 해제");
+#endif
+    }
+
+    public override void Update()
+    {
+        base.Update();
+        playerStateMachine.AnimatorInfo = playerStateMachine.Player.PlayerAnimator.GetCurrentAnimatorStateInfo(0);
+
+        //해당 State의 애니메이터와 연결 완료
+        if (!(playerStateMachine.IsCompareState))
+        {
+            if(playerStateMachine.AnimatorInfo.shortNameHash == playerStateMachine.Player.playerAnimationData.X_SkillAnimationHash)
+            {
+                playerStateMachine.IsCompareState = true;
+            }
+            else
+            {
+                playerStateMachine.SkipAttackAction?.Invoke();
+                return;
+            }
+        }
+        if(playerStateMachine.AnimatorInfo.normalizedTime < 1f) return;
+        playerStateMachine.EndAttackAction?.Invoke();
+    }
+}
