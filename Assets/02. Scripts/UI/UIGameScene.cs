f496bd2c67b6fb9195e928848b1cefe59812b9d2
Jung Ha Na <73771420+onehana1@users.noreply.github.com>
2025-04-11 23:00:57 +0900

    [Fix] 플레이어 hp 테스트 연결 수정
    - 나중에 게임씬 들어갔는지 확인 후 게임씬 들어가면 bind 하는 방식으로 변경해야될듯

--- Assets/02. Scripts/UI/UIGameScene.cs
+++ Assets/02. Scripts/UI/UIGameScene.cs
@@ -6,8 +6,8 @@ using System.Threading.Tasks;
 using System.Runtime.CompilerServices;
 public class UIGameScene : MonoBehaviour
 {
-    [SerializeField] private Button settingsButton;
-
+    [SerializeField] private string playerName = "Player1";
+    [SerializeField] private string playerHpName = "PlayerHp";
     private async Task Awake() 
     {
         await UIManager.Instance.LoadAllUI(UIType.Popup);
@@ -18,7 +18,12 @@ public class UIGameScene : MonoBehaviour
 
         UIManager.Instance.CreateAllUI(UIType.GamePlay);
         UIManager.Instance.Init();
+
+        // 나중에 게임씬 들어갔는지 확인 후 게임씬 들어가면 bind 하는 방식으로 변경
+        UIBinder.Instance.Bind<HPTest, UIHealthBar, HealthPresenter>(playerName, playerHpName); // 플레이어 체력 바 바인딩
+
         // UIManager.Instance.OpenAllPermanent();
     }
+
 
 }
