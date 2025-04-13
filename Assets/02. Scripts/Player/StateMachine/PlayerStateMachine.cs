87889798614960477c23e7db33c15bce4963e3d4
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-04-13 16:33:50 +0900

    [Refactor] PlayerStateMachine.cs 수정
    각 State로 전환하는 구간의 조건문이 겹치는 조건들이 많고 가독성이 떨어지는 문제로 각 조건을 bool 메서드 형식으로 정리.
    
    각종 주석 추가

--- Assets/02. Scripts/Player/StateMachine/PlayerStateMachine.cs
+++ Assets/02. Scripts/Player/StateMachine/PlayerStateMachine.cs
@@ -11,12 +11,13 @@ public class PlayerStateMachine : StateMachine
     public PlayerWalkState WalkState { get; private set; }
     public PlayerJumpState JumpState { get; private set; }
     public PlayerFallState FallState { get; private set; }
+    public PlayerDieState DieState { get; private set; }
+
     public PlayerSkillZState SkillZState { get; private set; }
     public PlayerSkillXState SkillXState { get; private set; }
     public PlayerSkillAState SkillAState { get; private set; }
     public PlayerSkillSState SkillSState { get; private set; }
     public PlayerSkillDState SkillDState { get; private set; }
-    public PlayerDieState DieState { get; private set; }
 
     public StoppableAction SkipAttackAction = new();
     public StoppableAction EndAttackAction = new();
@@ -35,12 +36,13 @@ public class PlayerStateMachine : StateMachine
         WalkState = new PlayerWalkState(this);
         JumpState = new PlayerJumpState(this);
         FallState = new PlayerFallState(this);
+        DieState = new PlayerDieState(this);
+
         SkillZState = new PlayerSkillZState(this);
         SkillAState = new PlayerSkillAState(this);
         SkillSState = new PlayerSkillSState(this);
         SkillDState = new PlayerSkillDState(this);
         SkillXState = new PlayerSkillXState(this);
-        DieState = new PlayerDieState(this);
 
         SkipAttackAction.AddListener(ConnectJumpState);
         SkipAttackAction.AddListener(ConnectDashState);
@@ -62,29 +64,19 @@ public class PlayerStateMachine : StateMachine
         ApplyState applyState = skillData.applyState;
 
         if ((ApplyState.IdleState | applyState) == applyState)
-        {
-            IdleState.MoveAction.AddListener(() => ConnectAction(isAction(), state, skillData));
-        }
+            IdleState.MoveAction.AddListener(() => ConnectAttackAction(isAction(), state, skillData));
 
         if ((ApplyState.WalkState | applyState) == applyState)
-        {
-            WalkState.MoveAction.AddListener(() => ConnectAction(isAction(), state, skillData));
-        }
+            WalkState.MoveAction.AddListener(() => ConnectAttackAction(isAction(), state, skillData));
 
         if ((ApplyState.JumpState | applyState) == applyState)
-        {
-            JumpState.MoveAction.AddListener(() => ConnectAction(isAction(), state, skillData));
-        }
+            JumpState.MoveAction.AddListener(() => ConnectAttackAction(isAction(), state, skillData));
 
         if ((ApplyState.DashState | applyState) == applyState)
-        {
-            SkillZState.MoveAction.AddListener(() => ConnectAction(isAction(), state, skillData));
-        }
+            SkillZState.MoveAction.AddListener(() => ConnectAttackAction(isAction(), state, skillData));
 
         if ((ApplyState.FallState | applyState) == applyState)
-        {
-            FallState.MoveAction.AddListener(() => ConnectAction(isAction(), state, skillData));
-        }
+            FallState.MoveAction.AddListener(() => ConnectAttackAction(isAction(), state, skillData));
     }
 
 
@@ -95,7 +87,7 @@ public class PlayerStateMachine : StateMachine
     /// <param name="state">변환할 State</param>
     /// <param name="skillData">참고할 SkillData</param>
     /// <returns>true면 Action 종료, false면 Action 계속</returns>
-    private bool ConnectAction(bool isAction, IPlayerState state, SkillData skillData)
+    private bool ConnectAttackAction(bool isAction, IPlayerState state, SkillData skillData)
     {
         if (isAction && skillData.canUse)
         {
@@ -105,6 +97,39 @@ public class PlayerStateMachine : StateMachine
         return false;
     }
 
+    /// <summary>MoveDir.X의 값이 0인지 확인</summary>
+    private bool IsZeroMoveDirX() => Player.input.MoveDir.x == 0;
+
+    /// <summary>MoveDir의 X값이 0인지 확인</summary>
+    private bool IsZeroMoveDir() => Player.input.MoveDir == Vector2.zero;
+
+    /// <summary>점프가 가능한 상태인지 확인</summary>
+    private bool IsCanJump() => Player.playerCheckGround.CanJump;
+
+    /// <summary>isTrigger가 어떤 상태인지 확인</summary>
+    private bool IsTrigger() => Player.playerGroundCollider.isTrigger;
+
+    /// <summary>isTrigger에 값을 대입</summary>
+    private void IsTrigger(bool setTrigger) => Player.playerGroundCollider.isTrigger = setTrigger;
+
+    /// <summary>velocity.y의 값이 0에 가까운지 확인</summary>
+    private bool IsZeroVelocityY() => Mathf.Approximately(Player.playerRigidbody.velocity.y, 0);
+
+    /// <summary>현재 닿고 있는 땅이 있는지 확인</summary>
+    private bool IsZeroGround() => (Player.playerCheckGround.GroundPlaneCount + Player.playerCheckGround.GroundPlatformCount) == 0;
+
+    /// <summary>해당 SlotKey의 스킬이 사용 가능한지 확인</summary>
+    private bool IsSkillCanUse(SkillSlotKey slotKey) => Player.equippedSkills[slotKey].canUse;
+
+    /// <summary>MoveDir.y의 값이 0보다 작은지 확인</summary>
+    private bool IsDownMoveDirY() => Player.input.MoveDir.y < 0;
+
+    /// <summary>현재 대시 카운트가 0보다 큰지 확인</summary>
+    private bool IsCanDashCount() => Player.playerData.PlayerAirData.CurDashCount > 0;
+
+    /// <summary>현재 닿고 있는 GroundPlane이 0인지 확인</summary>
+    private bool IsZeroGroundPlane() => Player.playerCheckGround.GroundPlaneCount == 0;
+
 
     /// <summary>
     /// Walk State에 진입 가능 여부를 확인하고 전환하는 메서드
@@ -112,12 +137,12 @@ public class PlayerStateMachine : StateMachine
     /// <returns>true면 Action 종료, false면 Action 계속</returns>
     public bool ConnectWalkState()
     {
-        if (Player.input.MoveDir.x != 0f &&
-            Player.playerCheckGround.CanJump &&
-            !(Player.playerGroundCollider.isTrigger) &&
-            Mathf.Approximately(Player.playerRigidbody.velocity.y, 0))
+        if (!(IsZeroMoveDirX()) &&          //움직이는 입력값 X가 0이 아니라면
+            IsCanJump() &&                  //점프가 가능한 상태라면
+            !(IsTrigger()) &&               //IsTrigger가 비활성화 상태라면
+            IsZeroVelocityY())              //velocitty Y의 힘이 0이라면
         {
-            ChangeState(WalkState);
+            ChangeState(WalkState);         //WalkState로 전환
             return true;
         }
         return false;
@@ -130,9 +155,9 @@ public class PlayerStateMachine : StateMachine
     /// <returns>true면 Action 종료, false면 Action 계속</returns>
     public bool ConnectFallState()
     {
-        if (!(Player.playerCheckGround.CanJump))
+        if (!(IsCanJump()))                 //점프가 가능하지 않다면
         {
-            ChangeState(FallState);
+            ChangeState(FallState);         //FallState로 전환
             return true;
         }
         return false;
@@ -145,15 +170,14 @@ public class PlayerStateMachine : StateMachine
     /// <returns>true면 Action 종료, false면 Action 계속</returns>
     public bool ConnectDashState()
     {
-        if (Player.equippedSkills[SkillSlotKey.Z].canUse &&
-            Player.input.IsSkillZ &&
-            (Player.input.MoveDir.x != 0 ||
-            Player.input.MoveDir.y > 0 ||
-            (Player.playerCheckGround.GroundPlaneCount + Player.playerCheckGround.GroundPlatformCount == 0) && 
-            Player.input.MoveDir != Vector2.zero) &&
-            Player.playerData.PlayerAirData.CurDashCount > 0)
+        if (IsZeroMoveDir()) return false;                  //아무런 이동 키를 입력하고 있지 않다면 검사 취소
+
+        if (Player.input.IsSkillZ &&                        //해당 스킬 키를 입력하고 있다면
+            IsSkillCanUse(SkillSlotKey.Z) &&                //해당 스킬이 사용가능하다면
+            (!(IsDownMoveDirY()) || IsZeroGround()) &&      //공중에 뜬 상태거나 아래키를 누르고 있지 않다면
+            IsCanDashCount())                               //대시 카운트가 존재한다면
         {
-            ChangeState(SkillZState);
+            ChangeState(SkillZState);                       //Z스킬 State로 전환
             return true;
         }
         return false;
@@ -166,20 +190,20 @@ public class PlayerStateMachine : StateMachine
     /// <returns>true면 Action 종료, false면 Action 계속</returns>
     public bool ConnectJumpState()
     {
-        if(Player.input.IsJump &&
-            Player.playerCheckGround.CanJump &&
-            Mathf.Approximately(Player.playerRigidbody.velocity.y, 0))
+        if(Player.input.IsJump &&           //점프키를 입력했다면
+            IsCanJump() &&                  //점프가 가능한 상태라면
+            IsZeroVelocityY())              //Y축으로 이동하는 힘이 0이라면
         {
-            if (Player.input.MoveDir.y >= 0)
+            if (!(IsDownMoveDirY()))        //아래키를 입력하고 있지 않다면
             {
-                ChangeState(JumpState);
+                ChangeState(JumpState);     //JumpState로 전환
                 return true;
             }
             
-            if(Player.playerCheckGround.GroundPlaneCount == 0)
+            if(IsZeroGroundPlane())         //닿고 있는 GroundPlane의 갯수가 0이라면
             {
-                Player.playerGroundCollider.isTrigger = true;
-                ChangeState(FallState);
+                IsTrigger(true);            //isTrigger을 true로 전환
+                ChangeState(FallState);     //FallState로 전환 
                 return true;
             }
         }
@@ -193,12 +217,12 @@ public class PlayerStateMachine : StateMachine
     /// <returns>true면 Action 종료, false면 Action 계속</returns>
     public bool ConnectIdleState()
     {
-        if (Player.input.MoveDir.x == 0 &&
-            Player.playerCheckGround.CanJump &&
-            !(Player.playerGroundCollider.isTrigger) &&
-            Mathf.Approximately(Player.playerRigidbody.velocity.y, 0))
+        if (IsZeroMoveDirX() &&         //움직이는 입력값X가 0이라면
+            IsCanJump() &&              //점프가 가능한 상태라면
+            !(IsTrigger()) &&           //Trigger가 비활성화 상태라면
+            IsZeroVelocityY())          //velocityY값이 0이라면
         {
-            ChangeState(IdleState);
+            ChangeState(IdleState);     //IdleState로 전환
             return true;
         }
         return false;
