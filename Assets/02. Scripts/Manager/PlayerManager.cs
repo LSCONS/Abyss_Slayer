d85ae87ccf2da410f1e47dd8b1831c2182403b14
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-04-13 16:31:06 +0900

    [Refactor] PlayerManager.cs 추가
    PlayerAnimationData를 각 Player가 아닌, PlayerManager에서 가지고 있도록 추가.

--- Assets/02. Scripts/Manager/PlayerManager.cs
+++ Assets/02. Scripts/Manager/PlayerManager.cs
@@ -1,8 +1,17 @@
+using Mono.Cecil;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerManager : Singleton<PlayerManager>
 {
+    public PlayerAnimationData PlayerAnimationData {  get; private set; }
 
+    protected override void Awake()
+    {
+        base.Awake();
+        DontDestroyOnLoad(gameObject);
+        PlayerAnimationData = Resources.Load<PlayerAnimationData>("Player/PlayerAnimationData/PlayerAnimationData");
+        PlayerAnimationData.Init();
+    }
 }
