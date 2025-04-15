ee6a25cc717783905cdf88e167609db3839c11c6
leewootak <leewt32414@gmail.com>
2025-04-15 16:41:25 +0900

    [Fix] 아처 스킬 코드 정리

--- Assets/02. Scripts/Skill/Skills/Mage/MageSkill_a.cs
+++ Assets/02. Scripts/Skill/Skills/Mage/MageSkill_a.cs
@@ -3,16 +3,35 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Skill/Mage/Mage_a")]
 public class MageSkill_a : SkillExecuter
 {
-    public int count;
     public int damage;
-    public float speed;
-    public float homingPower;
+
+    [Header("발사정보")]
+    public int count;
+    public float startCircleR;
     public float fireDegree;
     public float spreadDegree;
 
+    [Header("투사체 정보")]
+    public float homingPower;
+    public float speed;
+    public float homingTime;
+    public AnimationCurve homingCurve;
+
     public override void Execute(Player user, Player target, SkillData skillData)
     {
-        var homingProjectile = PoolManager.Instance.Get<HomingProjectile>();
-        homingProjectile.Init(damage, user.transform.position, user.transform.rotation, target.transform, speed, homingPower);
+        // for(int i = 0; i < count; i++)
+        // {
+        //     float degree = fireDegree - ((spreadDegree / 2) - (i * spreadDegree / (count - 1)));
+        //     degree = isLeft ? 180 - degree : degree;
+        //     Quaternion rotate = Quaternion.Euler(0, 0, degree); //오일러 각도로 각도계산
+
+        //     degree *= Mathf.Deg2Rad;                            //오일러 각도를 라디우스로 변환
+        //     Vector3 position = new Vector3(Mathf.Cos(degree), Mathf.Sin(degree)) * startCircleR;   //라디우스로 위치계산
+        //     position = bossTransform.TransformPoint(position);
+
+        //     PoolManager.Instance.Get<HomingProjectile>().Init(damage, user.transform.position, user.transform.rotation, target.transform, speed, homingPower, homingTime, homingCurve);
+
+        //     yield return new WaitForSeconds(0.1f);
+        // }
     }
 }
