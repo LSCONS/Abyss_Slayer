b670df47ec8eb636dbdb9791a1b9d1895f92da09
leewootak <leewt32414@gmail.com>
2025-04-15 12:56:30 +0900

    [Feat] 메이지 a 스킬 로직 구현중
    # MageSkill_a.cs
    - 메이지 유도탄 스킬 스크립트
    
    # MageProjectile.cs
    - 메이지 투사체 관리 스크립트

--- Assets/02. Scripts/Skill/Skills/Mage/MageSkill_a.cs
+++ Assets/02. Scripts/Skill/Skills/Mage/MageSkill_a.cs
@@ -3,7 +3,16 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Skill/Mage/Mage_a")]
 public class MageSkill_a : SkillExecuter
 {
+    public int count;
+    public int damage;
+    public float speed;
+    public float homingPower;
+    public float fireDegree;
+    public float spreadDegree;
+
     public override void Execute(Player user, Player target, SkillData skillData)
     {
+        var homingProjectile = PoolManager.Instance.Get<HomingProjectile>();
+        homingProjectile.Init(damage, user.transform.position, user.transform.rotation, target.transform, speed, homingPower);
     }
 }
