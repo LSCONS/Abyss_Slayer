d004d856d6613c1b25b14337669ce00a7caa2aa7
Jung Ha Na <73771420+onehana1@users.noreply.github.com>
2025-04-15 20:53:55 +0900

    [Feat] BuffSlotPresenter 스크립트 추가
    - 버프 슬롯 UI와 플레이어의 CurDuration 값을 연결 -> 실시간으로 쿨타임을 표시
    - 지속시간 종료되면 자동으로 UI를 제거
    - Presenter 클래스

---
+++ Assets/02. Scripts/UI/BuffSlotPresenter.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+public class BuffSlotPresenter : IPresenter
+{
+    private Player model;
+    private UIBuffSlot view;
+    private CompositeDisposable disposable = new();
+
+    public BuffSlotPresenter(Player model, UIBuffSlot view)
+    {
+        this.model = model;
+        this.view = view;
+
+        view.SetIcon(GetBuffIcon()); // 버프 아이콘 설정
+        view.SetCoolTime(model.CurDuration.Value / model.MaxDuration.Value);
+        view.SetPresenter(this);
+
+        model.CurDuration
+            .CombineLatest(model.MaxDuration, (cur, max) => cur / max)
+            .Subscribe(view.SetCoolTime)
+            .AddTo(disposable);
+
+        model.CurDuration
+            .Where(cur => cur <= 0)
+            .Take(1)
+            .Subscribe(_ =>
+            {
+                Object.Destroy(view.gameObject);
+                Dispose();
+            })
+            .AddTo(disposable);
+    }
+
+    private Sprite GetBuffIcon()
+    {
+        // 필요 시, 버프 발동한 스킬의 아이콘 반환
+        foreach (var skill in model.equippedSkills.Values)
+        {
+            if (skill.category == SkillCategory.Buff)
+                return skill.icon;
+        }
+
+        return null;
+    }
+
+    public void Dispose()
+    {
+        disposable.Dispose();
+    }
+
+}
