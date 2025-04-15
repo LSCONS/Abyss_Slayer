64065c9a27f26e9b09659fcf101d9a683e3b97e3
Jung Ha Na <73771420+onehana1@users.noreply.github.com>
2025-04-15 16:49:20 +0900

    [Feat] DOTween을 이용한 hp 애니메이션 추가
    # UIHealthBar.cs
    - SetHp에 매개변수 추가, hp와 maxhp도 받도록 수정
    
    # HealthPresenter.cs
    - view.SetHp(value.ratio, value.hp, value.maxHp) 프리젠터에 전달 추가

--- Assets/02. Scripts/UI/UIHealthBar.cs
+++ Assets/02. Scripts/UI/UIHealthBar.cs
@@ -2,27 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UniRx;
-
+using DG.Tweening;
+using TMPro;
 public class UIHealthBar : UIBase, IView
 {
     [SerializeField] private Image hpBar;
     [SerializeField] private IPresenter presenter;
-
-
-    private void OnValidate() {
-        hpBar = transform.GetGameObjectSameNameDFS("HpBar").GetComponent<Image>();
-    }
+    [SerializeField] private TextMeshProUGUI hpText;
 
     public override void Init()
     {
         hpBar.fillAmount = 1;
+        hpText.text = $"{100:F0}%";
     }
 
-    public void SetHp(float value)
+    private int currentHp = 0;  // 애니메이션에 쓰일 현재 hp
+    private Tween hpTween;  // 이전 Tween 저장
+    public void SetHp(float ratio, int hp, int maxHp)
     {
-        hpBar.fillAmount = value;
-    }
+        hpBar.DOFillAmount(ratio, 0.3f).SetEase(Ease.OutQuart);
+
+        // 이전 Tween 중지
+        hpTween?.Kill();
+
+        // 숫자 애니메이션
+        hpTween = DOTween.To(() => currentHp, x =>
+        {
+            currentHp = x;
+            hpText.text = $"{currentHp}/{maxHp}";
+        }, hp, 0.3f).SetEase(Ease.OutQuart);
+    }   
 
     public void SetPresenter(IPresenter presenter)
     {
