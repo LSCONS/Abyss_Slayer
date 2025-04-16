017b6ecfa67f4c40ecb754f452afc55b8a50ff56
Jung Ha Na <73771420+onehana1@users.noreply.github.com>
2025-04-16 19:16:56 +0900

    [Fix] 스킬 그룹 ui 정렬 오류 수정
    - DelayRebuildLayout 추가해서 한 프레임 대기 후 강제 정렬

--- Assets/02. Scripts/UI/UISkillSlotGroup.cs
+++ Assets/02. Scripts/UI/UISkillSlotGroup.cs
@@ -1,12 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UISkillSlotGroup : UIPermanent
 {
     public override void Init()
     {
         this.gameObject.SetActive(true);
-
+        StartCoroutine(DelayRebuildLayout());
+    }
+    private IEnumerator DelayRebuildLayout()
+    {
+        yield return null; // 한 프레임 대기
+        var layout = GetComponent<HorizontalLayoutGroup>();
+        if (layout != null)
+        {
+            LayoutRebuilder.ForceRebuildLayoutImmediate(layout.GetComponent<RectTransform>());
+        }
     }
 }
