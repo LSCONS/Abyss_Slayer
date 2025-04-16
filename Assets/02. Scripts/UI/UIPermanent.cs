fe4dc5d000a3816d818f8be50773a1b3d81aec0a
Jung Ha Na <73771420+onehana1@users.noreply.github.com>
2025-04-16 17:59:45 +0900

    [Feat] 고정 ui의 베이스인 UIPermanent 스크립트 추가
    - UIType 타입에 따라서 애니메이션 추가
       - top이면 위에서, bottom이면 아래서 등장

---
+++ Assets/02. Scripts/UI/UIPermanent.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class UIPermanent : UIBase
+{
+    public override void Init()
+    {
+        base.Init();
+        // 시작 위치 설정
+        var rect = GetComponent<RectTransform>();
+        var originalPos = rect.anchoredPosition;
+
+        Vector2 startPos = originalPos;
+
+        if ((uiType & UIType.Top) != 0)
+            startPos += new Vector2(0, 200f);
+        else if ((uiType & UIType.Bottom) != 0)
+            startPos += new Vector2(0, -200f);
+
+        rect.anchoredPosition = startPos;
+
+        // 등장 애니메이션
+        gameObject.SetActive(true); // 애니메이션 보기 위해 켜야 함
+
+        rect.DOAnchorPos(originalPos, 0.5f)
+            .SetEase(Ease.OutExpo);
+    }
+}
