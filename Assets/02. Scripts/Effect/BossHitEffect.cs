7495d3e4b17a707f4237465ddffa1b9bbf7753d2
Jung Ha Na <73771420+onehana1@users.noreply.github.com>
2025-04-17 23:03:52 +0900

    [Feat] BossHitEffect 보스에 생성되는 풀링 이펙트 추가
    - AutoReturn 으로 자동으로 일정시간 이후 풀에 반환 풀 객체 아니면 삭제

---
+++ Assets/02. Scripts/Effect/BossHitEffect.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossHitEffect : BasePoolable
+{
+
+    public override void Init()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public override void AutoReturn(float aliveTime)
+    {
+        StartCoroutine(EnableForAliveTime(aliveTime));
+    }
+
+    /// <summary>
+    /// 풀 객체인지 확인해서 풀 객체면 풀에 반환, 풀 객체가 아니면 그냥 파괴 (aliveTime 후에)
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator EnableForAliveTime(float aliveTime)
+    {
+        yield return new WaitForSeconds(aliveTime);
+        Debug.Log($"aliveTime: {aliveTime}");
+
+        // 풀 객체인지 확인해서 풀 객체면 풀에 반환, 풀 객체가 아니면 그냥 파괴 (aliveTime 후에)
+        var poolable = GetComponent<BasePoolable>();
+        if (poolable != null)
+        {
+            Debug.Log("ReturnToPool");
+            poolable.ReturnToPool();
+        }
+        else    // 풀 객체가 아니면 그냥 파괴
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+
+}
