21f87a5c86d4d83a46ba7e9cf6259cbd4fee998c
leewootak <leewt32414@gmail.com>
2025-04-17 17:21:04 +0900

    [Fix] 아처 스킬 수치 조절

--- Assets/02. Scripts/Skill/Refactory/SkillBase/RangeAttackSkill.cs
+++ Assets/02. Scripts/Skill/Refactory/SkillBase/RangeAttackSkill.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class RangeAttackSkill : Skill
 {
-    [field: SerializeField] public float Damage { get; private set; } = 10f;    //데미지
-    [field: SerializeField] public float Range { get; private set; } = 5f;      //사거리
-    [field: SerializeField] public float Speed { get; private set; } = 5f;      //투사체 속도
-    [field: SerializeField] public int SpriteNum { get; private set; } = 0;     //Sprite 인덱스 번호
+    [field: SerializeField] public float Damage { get; private set; }     //데미지
+    [field: SerializeField] public float Range { get; private set; }      //사거리
+    [field: SerializeField] public float Speed { get; private set; }      //투사체 속도
+    [field: SerializeField] public int SpriteNum { get; private set; }    //Sprite 인덱스 번호
 
 
     public void ThrowProjectile<T>(Vector3 startPos, Vector3 dir) where T : BasePoolable
