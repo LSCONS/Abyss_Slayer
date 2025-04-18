c287d0bda818edd4261f6002c27c534e46fcb78e
leewootak <leewt32414@gmail.com>
2025-04-18 19:21:11 +0900

    [Feat] 메이지 일반 공격 데이터 생성 및 플레이어에 연결

---
+++ Assets/02. Scripts/Skill/Refactory/SkillAbatar/MageAttackSkill.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "MageAttackSkill", menuName = "SkillRefactory/Range/MageAttack")]
+public class MageAttackSkill : RangeAttackSkill
+{
+    public override void UseSkill()
+    {
+        base.UseSkill();
+        Vector3 dirX = new Vector3(PlayerFrontXNomalized() * 1.5f, 0 ,0);       // 플레이어 방향 계산
+        
+        Debug.DrawRay(PlayerPosition() + dirX, dirX * Range, Color.red);
+        RaycastHit2D hit = Physics2D.Raycast(PlayerPosition() + dirX, dirX, Range, LayerMask.GetMask("Enemy"));
+        if (hit.collider != null)
+        {
+            hit.collider.GetComponent<Boss>().Damage((int)Damage); // 데미지 전달
+        }
+    }
+}
