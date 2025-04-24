6529cd36f1731f7690d76aac7c831c7edb47ced0
Jung Ha Na <73771420+onehana1@users.noreply.github.com>
2025-04-25 02:04:09 +0900

    [Feat] 게임 스테이트 바꿔주는 버튼 ui 스크리븥 추가

---
+++ Assets/02. Scripts/UI/UIChangeGameStateButton.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIChangeGameStateButton : UIButton
+{
+    [Header("전환하고 싶은 스테이트를 선택해 보아요!")]
+    [SerializeField] GameStartState gameStartState;
+    public override void Init()
+    {
+        base.Init();
+        Debug.Log($"[UIChangeGameStateButton] Init 호출됨: {gameStartState}");
+        gameObject.SetActive(true);
+        button.onClick.AddListener(ChangeState);
+
+    }
+
+
+    private async void ChangeState()
+    {
+        switch (gameStartState)
+        {
+            case GameStartState.Intro:
+                await GameFlowManager.Instance.ChangeState(new IntroState());
+                break;
+            case GameStartState.Start:
+                await GameFlowManager.Instance.ChangeState(new StartState());
+                break;
+            case GameStartState.Lobby:
+                await GameFlowManager.Instance.GoToLobby();
+                break;
+            case GameStartState.Rest:
+                await GameFlowManager.Instance.GoToRestState();
+                break;
+            case GameStartState.Boss:
+                await GameFlowManager.Instance.GoToNextBoss();
+                break;
+            default:
+                Debug.LogWarning($"[UIChangeGameStateButton] 알 수 없는 상태: {gameStartState}");
+                break;
+        }
+    }
+
+}
