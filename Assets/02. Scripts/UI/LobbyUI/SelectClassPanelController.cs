4bde87de7243458ceabaa4451b4a6eb72a540a12
Jung Ha Na <73771420+onehana1@users.noreply.github.com>
2025-04-25 00:41:23 +0900

    [Feat] 플레이어가 클래스 정할 수 있도록 하는 스크립트 추가
    - 전체적으로 클래스 선택 UI 관리해줌
    - 슬롯 클릭 이벤트 구독함
    - 클래스 정보를 저장하고 플레이어 매니저에게 전달함
    - 클래스 정보로 설명창 업데이트 시킴

---
+++ Assets/02. Scripts/UI/LobbyUI/SelectClassPanelController.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UniRx;
+using UnityEngine;
+using UnityEngine.InputSystem.XR;
+using UnityEngine.UI;
+
+public class SelectClassPanelController : UIPopup
+{
+    [SerializeField] private Button selectButton;
+    [SerializeField] private Button closeButton;
+    [SerializeField] private TextMeshProUGUI descText;
+
+    private CompositeDisposable disposables = new CompositeDisposable();
+    private ClassSlotController controller;
+
+    private CharacterClass selectedCharacterClass;
+    public override void Init()
+    {
+        base.Init();
+        controller = GetComponentInChildren<ClassSlotController>();
+        if(!closeButton) closeButton = transform.GetGameObjectSameNameDFS("Close").GetComponent<Button>();
+        if (!selectButton) selectButton = transform.GetGameObjectSameNameDFS("Select").GetComponent<Button>();
+
+
+        controller.CreateClassSlots();  // 클래스 슬롯 생성
+    }
+
+    public override void Open(params object[] args)
+    {
+        base.Open();
+        closeButton.onClick.AddListener(OnClose);
+        selectButton.onClick.AddListener(OnSelect);
+
+        // class slot에서 발생한 이벤트를 구독하기
+        foreach (var slot in controller.Slots)
+        {
+            slot.OnClickAsObservable
+                .Subscribe(OnClassSelected)
+                .AddTo(disposables);
+        }
+    }
+
+    private void OnClassSelected(CharacterClass cc)
+    {
+        selectedCharacterClass = cc;
+
+        // 설명 갱신
+        descText.text = cc.GetDescription();
+    }
+
+    private void OnSelect()
+    {
+        PlayerManager.Instance.SetSelectedClass(selectedCharacterClass);
+        OnClose();
+    }
+
+    public override void OnClose()
+    {
+        Debug.Log("OnClose");
+        disposables.Clear();
+        base.OnClose();
+
+    }
+}
