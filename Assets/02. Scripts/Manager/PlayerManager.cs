aa768384c30ced0e86ea51b6a193d2652e29d6a1
Jung Ha Na <73771420+onehana1@users.noreply.github.com>
2025-04-25 00:32:23 +0900

    [Add] PlayerManager에 클래스 세팅해주는 메서드 추가

--- Assets/02. Scripts/Manager/PlayerManager.cs
+++ Assets/02. Scripts/Manager/PlayerManager.cs
@@ -7,6 +7,9 @@ public class PlayerManager : Singleton<PlayerManager>
 
     private Dictionary<CharacterClass, AnimatorOverrideController> animatorMap;    // 클래스마다 변경될 오버라이드 컨트롤러 가져야됨
 
+    [SerializeField, ReadOnly] private CharacterClass selectedCharacterClass;
+    public CharacterClass SelectedCharacterClass => selectedCharacterClass;
+
     protected override void Awake()
     {
         base.Awake();
@@ -29,5 +32,11 @@ public class PlayerManager : Singleton<PlayerManager>
             else return;
         }
         targetAnimator.runtimeAnimatorController = controller;
+    }
+
+    // 클래스 세팅해주는 메서드
+    public void SetSelectedClass(CharacterClass selectedCalss)
+    {
+        selectedCharacterClass = selectedCalss;
     }
 }
