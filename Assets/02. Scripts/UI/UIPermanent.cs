6edbc8dea2131d1f00e90614fe1e028d936e0789
Jung Ha Na <73771420+onehana1@users.noreply.github.com>
2025-04-28 19:15:31 +0900

    [Fix] UIPermanent 두트윈 문제 주석처리

--- Assets/02. Scripts/UI/UIPermanent.cs
+++ Assets/02. Scripts/UI/UIPermanent.cs
@@ -8,27 +8,28 @@ public class UIPermanent : UIBase
     public override void Init()
     {
         base.Init();
+
         // 시작 위치 설정
-        var rect = GetComponent<RectTransform>();
-        var originalPos = rect.anchoredPosition;
+        //var rect = GetComponent<RectTransform>();
+        //var originalPos = rect.anchoredPosition;
 
-        Vector2 startPos = originalPos;
+        //Vector2 startPos = originalPos;
 
-        if ((uiType & UIType.Top) != 0)
-            startPos += new Vector2(0, 0f);
-        else if ((uiType & UIType.Bottom) != 0)
-            startPos += new Vector2(0, -0f);
-        else if ((uiType & UIType.TopMid) != 0)
-            startPos += new Vector2(0, 0f);
-        else if ((uiType & UIType.Permanent) != 0)  // 그냥 고정 ui면 애니메이션 없음
-                return;
+        //if ((uiType & UIType.Top) != 0)
+        //    startPos += new Vector2(0, 0f);
+        //else if ((uiType & UIType.Bottom) != 0)
+        //    startPos += new Vector2(0, -0f);
+        //else if ((uiType & UIType.TopMid) != 0)
+        //    startPos += new Vector2(0, 0f);
+        //else if ((uiType & UIType.Permanent) != 0)  // 그냥 고정 ui면 애니메이션 없음
+        //        return;
 
-        rect.anchoredPosition = startPos;
+        //rect.anchoredPosition = startPos;
 
         // 등장 애니메이션
         gameObject.SetActive(true); // 애니메이션 보기 위해 켜야 함
 
-        rect.DOAnchorPos(originalPos, 0.5f)
-            .SetEase(Ease.OutExpo);
+        //rect.DOAnchorPos(originalPos, 0.5f)
+        //    .SetEase(Ease.OutExpo);
     }
 }
