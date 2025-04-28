b32357a7584f095914c63e8f835e92612c3984e2
Jung Ha Na <73771420+onehana1@users.noreply.github.com>
2025-04-28 19:17:00 +0900

    [Fix] UISkillSlotGroup의 init에서 base.init 추가

--- Assets/02. Scripts/UI/UISkillSlotGroup.cs
+++ Assets/02. Scripts/UI/UISkillSlotGroup.cs
@@ -7,16 +7,7 @@ public class UISkillSlotGroup : UIPermanent
 {
     public override void Init()
     {
+        base.Init();
         this.gameObject.SetActive(true);
-        StartCoroutine(DelayRebuildLayout());
-    }
-    private IEnumerator DelayRebuildLayout()
-    {
-        yield return null; // 한 프레임 대기
-        var layout = GetComponent<HorizontalLayoutGroup>();
-        if (layout != null)
-        {
-            LayoutRebuilder.ForceRebuildLayoutImmediate(layout.GetComponent<RectTransform>());
-        }
     }
 }
