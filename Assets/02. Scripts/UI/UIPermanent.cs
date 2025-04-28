5e3f524b806be269dc324cb9941f9726bc867a0d
Jung Ha Na <73771420+onehana1@users.noreply.github.com>
2025-04-28 19:06:10 +0900

    [Fix] UI 위로 올라가는 오류 수정
    - 두트윈 삭제해서 오류 수정

--- Assets/02. Scripts/UI/UIPermanent.cs
+++ Assets/02. Scripts/UI/UIPermanent.cs
@@ -15,11 +15,11 @@ public class UIPermanent : UIBase
         Vector2 startPos = originalPos;
 
         if ((uiType & UIType.Top) != 0)
-            startPos += new Vector2(0, 200f);
+            startPos += new Vector2(0, 0f);
         else if ((uiType & UIType.Bottom) != 0)
-            startPos += new Vector2(0, -200f);
+            startPos += new Vector2(0, -0f);
         else if ((uiType & UIType.TopMid) != 0)
-            startPos += new Vector2(0, 200f);
+            startPos += new Vector2(0, 0f);
         else if ((uiType & UIType.Permanent) != 0)  // 그냥 고정 ui면 애니메이션 없음
                 return;
 
