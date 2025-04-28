59f7608d4fec21a2e12fa817c602042bd066adf9
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-04-28 20:23:59 +0900

    [Fix] 플레이어의 탄환이 플레이어가 맞는 문제 해결

--- Assets/02. Scripts/Projectile/PlayerProjectile/MageProjectile.cs
+++ Assets/02. Scripts/Projectile/PlayerProjectile/MageProjectile.cs
@@ -86,7 +86,7 @@ public class MageProjectile : BasePoolable
     // 타겟과 충돌 시 행동
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.TryGetComponent<IHasHealth>(out IHasHealth enemy))
+        if (collision.TryGetComponent<IHasHealth>(out IHasHealth enemy) && enemy is Boss)
         {
             trailRenderer.enabled = false; // 투사체 궤적 비활성화
             fired = false; // 발사 여부 초기화
