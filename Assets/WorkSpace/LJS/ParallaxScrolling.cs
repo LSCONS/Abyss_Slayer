784fd1bc021ce28e64fe45d8315d774a719e6293
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-05-01 20:17:08 +0900

    [Fix] 맵 이동 관련 카메라가 아닌 플레이어를 중심으로 이동하도록 변경

--- Assets/WorkSpace/LJS/ParallaxScrolling.cs
+++ Assets/WorkSpace/LJS/ParallaxScrolling.cs
@@ -26,11 +26,8 @@ public class ParallaxScrolling : MonoBehaviour
 
     private void SetCamera()
     {
-        // 현재 메인 카메라를 찾음
-        cam = Camera.main.transform;
-
         // 시작 시점에 카메라 위치 저장
-        previousCamPos = cam.position;
+        previousCamPos = PlayerManager.Instance.Player.transform.position;
     }
     /// <summary>
     /// 배경 이미지 움직이게하는 메서드
@@ -38,13 +35,13 @@ public class ParallaxScrolling : MonoBehaviour
     private void ParallaxBackground()
     {
         // 현재 카메라 위치와 이전 위치의 차이 계산
-        Vector3 deltaMovement = cam.position - previousCamPos;
+        Vector3 deltaMovement = PlayerManager.Instance.Player.transform.position - previousCamPos;
         // 해당 차이값에 parallaxSpeed를 곱해서 배경을 이동시킴
         // x와 y 방향 모두 적용 가능 (예: 위아래 움직임도 가능)
         transform.position += new Vector3(deltaMovement.x * parallaxSpeed,
                                           deltaMovement.y * parallaxSpeed * 0,
                                           0);
         // 다음 프레임을 위해 현재 카메라 위치를 저장해둠
-        previousCamPos = cam.position;
+        previousCamPos = PlayerManager.Instance.Player.transform.position;
     }
 }
