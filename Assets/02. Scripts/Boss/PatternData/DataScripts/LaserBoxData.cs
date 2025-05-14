fd274d8f9d7e18a196a9aa125f73cf3ba9dc3293
Kim199610 <kk2247401@naver.com>
2025-05-14 21:00:20 +0900

    [feat]최종보스 패턴추가/패턴 일부 수정

--- Assets/02. Scripts/Boss/PatternData/DataScripts/LaserBoxData.cs
+++ Assets/02. Scripts/Boss/PatternData/DataScripts/LaserBoxData.cs
@@ -5,24 +5,30 @@ using UnityEngine;
 public class LaserBoxData : BasePatternData
 {
     [SerializeField] int damage;
+    [SerializeField] bool isPiercing;
+    [SerializeField] int fireCount = 3;
     [SerializeField] float preDelayTime = 1.5f;
+    [SerializeField] Vector3 spawnPoint;
     [SerializeField] List<Vector3> firePositions = new List<Vector3>();
+    [SerializeField] float spawnTime = 0.3f;
     [SerializeField] float moveTime = 1.2f;
     [SerializeField] float chasingTime = 1f;
-    [SerializeField] float delayTime = 0.3f;
     [SerializeField] float postDelayTime = 5f;
+    
 
     public override IEnumerator ExecutePattern()
     {
-        bossAnimator.SetTrigger("LaserBox1");
+        bossController.isLeft = bossTransform.position.x > 0;
+        bossAnimator.SetTrigger("Spawn");
         yield return new WaitForSeconds(preDelayTime);
 
         for (int i = 0; i < firePositions.Count; ++i)
         {
             Vector3 firePosition = firePositions[i] + (Vector3.up * Random.Range(-3, 3) + Vector3.right * Random.Range(-3, 3));
-            PoolManager.Instance.Get<LaserBoxProjectile>().Init(damage, target, bossTransform.position + Vector3.up * 2, 1.5f, firePosition, moveTime, chasingTime, delayTime, 3);
+            PoolManager.Instance.Get<LaserBoxProjectile>().Init(damage, target, bossTransform.position, 1.5f, firePosition, moveTime, chasingTime, spawnTime, isPiercing, fireCount);
         }
-        
+        yield return new WaitForSeconds(spawnTime);
+        bossAnimator.SetTrigger("Throw");
         yield return new WaitForSeconds(postDelayTime);
     }
 }
