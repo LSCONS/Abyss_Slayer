40b49d887c790df8325e34111471017216ba2300
Jung Ha Na <73771420+onehana1@users.noreply.github.com>
2025-05-14 23:19:53 +0900

    [Fix] 씬 넘어갈 때마다 스킬 상점의 스킬슬롯 늘어나는 오류 수정
    - UISkillUpgradeController의 Init 부분 리펙토링

--- Assets/02. Scripts/UI/UISkillUpgradeController.cs
+++ Assets/02. Scripts/UI/UISkillUpgradeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using TMPro;
 using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -14,58 +15,75 @@ public class UISkillUpgradeController : UIPopup
     [SerializeField] private TextMeshProUGUI skillPointText;
     [ReadOnlyAttribute] private int skillPoint = 0;
 
-
-
     private Dictionary<Skill, UISkillSlot> upgradeSlots = new();
 
     public override void Init()
     {
         base.Init();
 
-        skillPoint = PlayerManager.Instance.Player.SkillPoint;
-        UpdateSkillPointText();
+        // 슬롯이 이미 생성되어 있으면 초기화 생략해야됨
+        if(upgradeSlots.Count > 0)
+        {
+            skillPoint = PlayerManager.Instance.Player.SkillPoint;
+            UpdateSkillPointText();
+            return;
+        }
 
         foreach (var skill in PlayerManager.Instance.Player.EquippedSkills.Values)
         {
             var go = Instantiate(skillSlotPrefab, upgradeSlotParent);
             var slot = go.GetComponent<UISkillSlot>();
 
-            // 슬롯 세팅
-            slot.SetSkillData(skill);
-            slot.SetIcon(skill.SkillIcon);
-            slot.SetName(skill);
-            slot.SetSkillLevel(skill.Level.Value);
-
-            // 버튼 세팅
-            var buttons = slot.GetComponentsInChildren<Button>();
-            var upgradeButton = buttons[0];
-            var downgradeButton = buttons[1];
-
-
-            upgradeButton.onClick.AddListener(() =>
-            {
-                if (skillPoint > 0)
-                {
-                    skill.SkillUpgrade();
-                    skillPoint--;
-                    slot.SetSkillLevel(skill.Level.Value);
-                    UpdateSkillPointText();
-                }
-            });
-
-            downgradeButton.onClick.AddListener(() =>
-            {
-                if (skill.Level.Value > 1)
-                {
-                    skill.Level.Value--;
-                    skillPoint++;
-                    slot.SetSkillLevel(skill.Level.Value);
-                    UpdateSkillPointText();
-                }
-            });
-
-            upgradeSlots.Add(skill, slot);
+            SetSlots(slot, skill);      // 슬롯 세팅
+            SetButtons(slot, skill);    // 버튼 연결
         }
+    }
+
+    /// <summary>
+    /// 슬롯 세팅
+    /// </summary>
+    private void SetSlots(UISkillSlot slot, Skill skill)
+    {
+        slot.SetSkillData(skill);
+        slot.SetIcon(skill.SkillIcon);
+        slot.SetName(skill);
+        slot.SetSkillLevel(skill.Level.Value);
+    }
+
+    /// <summary>
+    /// 버튼 세팅
+    /// </summary>
+    private void SetButtons(UISkillSlot slot, Skill skill)
+    {
+        // 버튼 세팅
+        var buttons = slot.GetComponentsInChildren<Button>();
+        var upgradeButton = buttons[0];
+        var downgradeButton = buttons[1];
+
+
+        upgradeButton.onClick.AddListener(() =>
+        {
+            if (skillPoint > 0)
+            {
+                skill.SkillUpgrade();
+                skillPoint--;
+                slot.SetSkillLevel(skill.Level.Value);
+                UpdateSkillPointText();
+            }
+        });
+
+        downgradeButton.onClick.AddListener(() =>
+        {
+            if (skill.Level.Value > 1)
+            {
+                skill.Level.Value--;
+                skillPoint++;
+                slot.SetSkillLevel(skill.Level.Value);
+                UpdateSkillPointText();
+            }
+        });
+
+        upgradeSlots.Add(skill, slot);
     }
 
     private void UpdateSkillPointText()
