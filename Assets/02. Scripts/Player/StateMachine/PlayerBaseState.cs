c4d5198118d97aa3decdd4bc2aadb3466a22a994
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-05-20 19:52:11 +0900

    [Fix] 기본 상태 바뀌는 애니메이션 속도 변경

--- Assets/02. Scripts/Player/StateMachine/PlayerBaseState.cs
+++ Assets/02. Scripts/Player/StateMachine/PlayerBaseState.cs
@@ -6,7 +6,7 @@ public class PlayerBaseState : IPlayerState
 {
     protected PlayerStateMachine playerStateMachine;
     protected readonly PlayerGroundData playerGroundData;
-    public float ChangeSpriteTime { get; set; } = 0.2f;
+    public float ChangeSpriteTime { get; set; } = 0.06f;
     public float CurTime { get; set; }
 
     public PlayerBaseState(PlayerStateMachine playerStateMachine)
