ed40d13a5a93e42a54b703b33148b6c6e50aa4d5
Kim199610 <kk2247401@naver.com>
2025-05-22 14:56:17 +0900

    [feat]바위날리기 추가

--- Assets/02. Scripts/Boss/PatternData/DataScripts/ThrowRockData.cs
+++ Assets/02. Scripts/Boss/PatternData/DataScripts/ThrowRockData.cs
@@ -9,6 +9,7 @@ public class ThrowRockData : BasePatternData
     [SerializeField] float delayThrowTime = 0f;
     [SerializeField] float spawnPositionY = 3f;
     [SerializeField] float rockSpeed = 10f;
+    [SerializeField] float gravityScale = 1f;
     [SerializeField] float rockSize = 1f;
     [SerializeField] float postDelayTime = 1f;
     public override IEnumerator ExecutePattern()
@@ -20,7 +21,7 @@ public class ThrowRockData : BasePatternData
         yield return new WaitForSeconds(preDelayTime + 0.5f);
         //TODO: 나중에 애니메이션 트리거 추가 시 Rpc 추가
         //bossAnimator.SetTrigger("ThrowRock2");
-        ServerManager.Instance.InitSupporter.Rpc_StartGravityProjectileInit(damage, bossTransform.position + Vector3.up * spawnPositionY, rockSpeed, playerRef, delayThrowTime, int.MaxValue, rockSize, 1);
+        ServerManager.Instance.InitSupporter.Rpc_StartGravityProjectileInit(damage, bossTransform.position + Vector3.up * spawnPositionY, rockSpeed, playerRef, delayThrowTime, int.MaxValue, rockSize, gravityScale);
         //PoolManager.Instance.Get<GravityProjectile>().Init(damage, bossTransform.position + Vector3.up * 2, rockSpeed, target.position, int.MaxValue,rockSize, 1);
         bossController.ChasingTarget = false;
         bossController.ShowTargetCrosshair = false;
