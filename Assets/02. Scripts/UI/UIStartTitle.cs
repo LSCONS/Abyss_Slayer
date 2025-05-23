78cf57bfdfd81c29a4151133f94a6ebf2c2ffa81
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-05-23 22:12:53 +0900

    [Feat] 클리어 시 타이틀 화면에 인증 가능하도록 추가

--- Assets/02. Scripts/UI/UIStartTitle.cs
+++ Assets/02. Scripts/UI/UIStartTitle.cs
@@ -6,13 +6,20 @@ using UnityEngine;
 public class UIStartTitle : UIPermanent
 {
     [field: SerializeField] public TextMeshProUGUI TextName { get; private set;}
+    [field: SerializeField] public TextMeshProUGUI TextClearTime { get; private set;}
     private void Awake()
     {
         ServerManager.Instance.UIStartTitle = this;
+        ClearTimeUpdate();
     }
 
     public void TextUpdate()
     {
         TextName.text = ServerManager.Instance.PlayerName;
+    }
+
+    public void ClearTimeUpdate()
+    {
+        TextClearTime.text = GameValueManager.Instance.GameClearTime;
     }
 }
