13fedf4da38c7b29a45446b3f7d72a067b749e2d
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-05-28 04:27:28 +0900

    Revert "Merge branch 'Dev_Server' into SJW_LSCONS_Server2"
    This reverts commit a21a1ad28441205e37c79f660b31b24a967d5b96, reversing
    changes made to e04fc00e54c676f1ba940338c574efb13b85060e.

--- Assets/02. Scripts/Boss/BossController.cs
+++ Assets/02. Scripts/Boss/BossController.cs
@@ -82,7 +82,7 @@ public class BossController : NetworkBehaviour
 
     // 페이즈 관련
     private bool hasEnteredPhase2 = false;      // 2페 들갔나 확인
-    //[SerializeField] BasePatternData curPatternDebug;
+    [SerializeField] BossPattern curPatternDebug;
 
     public void Init()
     {
@@ -160,7 +160,7 @@ public class BossController : NetworkBehaviour
                     Target = GetClosestPlayerTransform();
 
                 Target = next.patternData.target;
-                //curPatternDebug = next.patternData;
+                curPatternDebug = next;
                 yield return StartCoroutine(next.patternData.ExecutePattern());
                 if (next.samePatternDelayTime > 0)
                     StartCoroutine(CheckDelayCompute(next));
@@ -175,8 +175,6 @@ public class BossController : NetworkBehaviour
     public void ReStartPatternLoop()
     {
         Invoke("Restart", 0.5f);
-        IsRun = false;
-        
     }
     void Restart()
     {
--- Assets/02. Scripts/Boss/PatternData/DataScripts/FoxSphere2Data.cs
+++ Assets/02. Scripts/Boss/PatternData/DataScripts/FoxSphere2Data.cs
@@ -2,10 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FoxSphere2Data : BasePatternData
+public class FoxSphere2Data : MonoBehaviour
 {
-    public override IEnumerator ExecutePattern()
+    // Start is called before the first frame update
+    void Start()
     {
-        throw new System.NotImplementedException();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
