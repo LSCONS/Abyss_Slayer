258443f3831d22ebe10b9f1abe94ad2b4380db16
Jung Ha Na <73771420+onehana1@users.noreply.github.com>
2025-05-28 12:33:34 +0900

    [WIP] 튜토리얼 데이터 추가중

---
+++ Assets/02. Scripts/Others/TutorialContoroller.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ETutorialStep
+{
+    Move,       // 상하좌우
+    Jump,       // C
+    Dash,       // Z
+    Attack,     // X
+    Skill,      // A/S/D
+    UsePoint,   // 스탯/스킬포인트 사용
+}
+public class TutorialContoroller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
